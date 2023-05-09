3da010413e9bc9aab68a8db80423265b3767f01b
Mike Hollingshaus <mike.haus.dev@gmail.com>
2023-05-09 08:04:41 -0600

    fix clerk provider auth error

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,11 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ClerkProvider } from "@clerk/nextjs/dist/components.server";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <CacheProvider>
-        <ChakraProvider>{children}</ChakraProvider>
-      </CacheProvider>
-    </ClerkProvider>
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
   );
 }
