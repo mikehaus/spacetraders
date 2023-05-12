3428b412af51b4fac8e099658bded0a6ca71fcfb
Mike Hollingshaus <mike.haus.dev@gmail.com>
2023-05-12 08:53:57 -0600

    remove chakra

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,12 @@
 "use client";
 
 import React from "react";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { CacheProvider } from "@chakra-ui/next-js";
 import { QueryClient, QueryClientProvider } from "react-query";
-
-const chakraTheme = extendTheme({});
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
 
   return (
-    <CacheProvider>
-      <ChakraProvider theme={chakraTheme}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </ChakraProvider>
-    </CacheProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
